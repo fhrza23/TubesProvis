325e71f18045d9a51df7c5470efee9869fa5838a
sghda <salmaghaida@upi.edu>
2024-05-24 08:51:48 +0700

    tambah future login di api service

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import '../../models/notif.dart';
-// import '../../models/user.dart';
+import '../../models/user.dart';
 
 class ApiService {
   final String baseUrl = "http://127.0.0.1:8000/api";
@@ -19,24 +19,24 @@ class ApiService {
   }
 
 
-  // Future<User> login(String nik, String password) async {
-  //   final response = await http.post(
-  //     Uri.parse('$baseUrl/login'),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: jsonEncode({
-  //       'nik': nik,
-  //       'password': password,
-  //     }),
-  //   );
+  Future<User> login(String nik, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/login'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: jsonEncode({
+        'nik': nik,
+        'password': password,
+      }),
+    );
 
-  //   if (response.statusCode == 200) {
-  //     // Asumsikan server mengembalikan user details dalam respons
-  //     return User.fromJson(jsonDecode(response.body));
-  //   } else {
-  //     // Tangani kesalahan
-  //     throw Exception('Failed to login');
-  //   }
-  // }
+    if (response.statusCode == 200) {
+      // Asumsikan server mengembalikan user details dalam respons
+      return User.fromJson(jsonDecode(response.body));
+    } else {
+      // Tangani kesalahan
+      throw Exception('Failed to login');
+    }
+  }
 }
